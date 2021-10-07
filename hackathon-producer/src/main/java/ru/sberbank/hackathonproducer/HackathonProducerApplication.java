7a66df7f2c4a64a1fc4901c23ae09a94de2cbf52
codemossaka <peyagodson@ymail.com>
2021-10-07 19:12:04 +0300

    docker compose

--- hackathon-producer/src/main/java/ru/sberbank/hackathonproducer/HackathonProducerApplication.java
+++ hackathon-producer/src/main/java/ru/sberbank/hackathonproducer/HackathonProducerApplication.java
@@ -1,13 +1,24 @@
 package ru.sberbank.hackathonproducer;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.sberbank.hackathonproducer.service.ProducerService;
 
 @SpringBootApplication
-public class HackathonProducerApplication {
+@RequiredArgsConstructor
+public class HackathonProducerApplication implements CommandLineRunner {
+
+	private final ProducerService producerService;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackathonProducerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		producerService.produce();
+	}
 }
