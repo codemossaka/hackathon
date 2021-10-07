7c4f173d26487cd03010ea4ac88287d4ecb2ffa8
codemossaka <peyagodson@ymail.com>
2021-10-07 23:30:00 +0300

    producer done

--- hackathon-consumer/src/main/java/ru/sberbank/hackathonconsumer/entities/Door.java
+++ hackathon-consumer/src/main/java/ru/sberbank/hackathonconsumer/entities/Door.java
@@ -1,6 +1,8 @@
 package ru.sberbank.hackathonconsumer.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,6 +11,8 @@ import javax.persistence.Table;
 @Data
 @Entity
 @Table(name="door")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Door {
 
     @Id
