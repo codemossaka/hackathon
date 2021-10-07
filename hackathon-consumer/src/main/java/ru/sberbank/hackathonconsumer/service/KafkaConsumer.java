25653fb296b01d6f1b6a79f0052ccd8de5443b8b
codemossaka <peyagodson@ymail.com>
2021-10-08 02:54:37 +0300

    producer done

--- hackathon-consumer/src/main/java/ru/sberbank/hackathonconsumer/service/KafkaConsumer.java
+++ hackathon-consumer/src/main/java/ru/sberbank/hackathonconsumer/service/KafkaConsumer.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import ru.sberbank.hackathonconsumer.dto.UserEventDto;
 import ru.sberbank.hackathonconsumer.entities.UserEvent;
@@ -22,13 +23,14 @@ public class KafkaConsumer {
     private final DoorRepository doorRepository;
     private final RoomRepository roomRepository;
     private final UserEventRepository userEventRepository;
-
+    private final SimpMessagingTemplate template;
     @KafkaListener(topics = "user-event", groupId = "group_id")
     public void consume(String message) {
         ObjectMapper mapper = new ObjectMapper();
         UserEventDto userEventDto = null;
         try {
             userEventDto = mapper.readValue(message, UserEventDto.class);
+            template.convertAndSend("/topic/userEvent", userEventDto);
         } catch (JsonProcessingException e) {
             log.warn("Невозможно распарсить событие из producer: " + message);
             throw new RuntimeException("Невозможно распарсить сообщение из producer: " + message);
