926df28b000a4548ceb90cb3fca75861b5d1db5d
Murtazali05 <murtuzali1996@yandex.ru>
2021-10-08 03:01:55 +0300

    Добавил движения для одного пользователя

--- hackathon-producer/src/main/java/ru/sberbank/hackathonproducer/HackathonProducerApplication.java
+++ hackathon-producer/src/main/java/ru/sberbank/hackathonproducer/HackathonProducerApplication.java
@@ -20,14 +20,60 @@ public class HackathonProducerApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		while (true){
-			Thread.sleep(2000);
+			// Пользователь с улицы заходить в лобби, из лобби в guest room
+			Thread.sleep(5000);
 			UserEvent userEvent = new UserEvent();
-			userEvent.setId((long) ((Math.random() * ((100 - 1) + 1)) + 1));
-			userEvent.setUserId((long) ((Math.random() * ((10 - 1) + 1)) + 1));
-			userEvent.setDoorId((long) ((Math.random() * ((5 - 1) + 1)) + 1));
-			userEvent.setEntryRoomId((long) ((Math.random() * ((6 - 1) + 1)) + 1));
-			userEvent.setExitRoomId((long) ((Math.random() * ((6 - 1) + 1)) + 1));
+			userEvent.setId(1L);
+			userEvent.setUserId(1L);
+			userEvent.setDoorId(1L);
+			userEvent.setEntryRoomId(2L);
+			userEvent.setExitRoomId(null);
 			producerService.produce(userEvent);
+			Thread.sleep(5000);
+//			из лобби в guest room
+			UserEvent userEvent1 = new UserEvent();
+			userEvent1.setId(1L);
+			userEvent1.setUserId(1L);
+			userEvent1.setDoorId(6L);
+			userEvent1.setEntryRoomId(6L);
+			userEvent1.setExitRoomId(2L);
+			producerService.produce(userEvent1);
+			//	из guest room в лобби
+			Thread.sleep(5000);
+			UserEvent userEvent2 = new UserEvent();
+			userEvent2.setId(1L);
+			userEvent2.setUserId(1L);
+			userEvent2.setDoorId(6L);
+			userEvent2.setEntryRoomId(2L);
+			userEvent2.setExitRoomId(6L);
+			producerService.produce(userEvent2);
+			// из лобби на 100A
+			Thread.sleep(5000);
+			UserEvent userEvent3 = new UserEvent();
+			userEvent3.setId(1L);
+			userEvent3.setUserId(1L);
+			userEvent3.setDoorId(2L);
+			userEvent3.setEntryRoomId(3L);
+			userEvent3.setExitRoomId(2L);
+			producerService.produce(userEvent3);
+			// из 100A в лобби
+			Thread.sleep(5000);
+			UserEvent userEvent4 = new UserEvent();
+			userEvent4.setId(1L);
+			userEvent4.setUserId(1L);
+			userEvent4.setDoorId(2L);
+			userEvent4.setEntryRoomId(2L);
+			userEvent4.setExitRoomId(3L);
+			producerService.produce(userEvent4);
+			// из лобби на свободу
+			Thread.sleep(5000);
+			UserEvent userEvent5 = new UserEvent();
+			userEvent5.setId(1L);
+			userEvent5.setUserId(1L);
+			userEvent5.setDoorId(1L);
+			userEvent5.setEntryRoomId(null);
+			userEvent5.setExitRoomId(2L);
+			producerService.produce(userEvent5);
 		}
 	}
 }
